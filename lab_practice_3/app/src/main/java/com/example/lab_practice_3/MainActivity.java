a8099172dd95b217396a4a89035fc80d957b1a5b
pheberaicheljohn <pheberaichaljohn@gmail.com>
2019-02-14 22:04:20 +0530

    half 3rd project

--- lab_practice_3/app/src/main/java/com/example/lab_practice_3/MainActivity.java
+++ lab_practice_3/app/src/main/java/com/example/lab_practice_3/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.lab_practice_3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b;
+    EditText e;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        b = (Button)findViewById(R.id.button);
+
+        e = (EditText)findViewById(R.id.editText);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),Main2Activity.class);
+                startActivity(i);
+            }
+        });
     }
+
+
 }
